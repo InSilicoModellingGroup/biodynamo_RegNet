6cb5c1da4c04f952d2841818bcb259d53d3ce7b4
Vasileios Vavourakis <vasvav@gmail.com>
2026-02-02 16:36:02 +0200

    Simplified the code with respect to the model parameters of the ODE solver for the regulatory network

--- src/biodynamo_RegNet.h
+++ src/biodynamo_RegNet.h
@@ -22,34 +22,49 @@ namespace bdm {
 
 enum Substances { kCytokine };
 
-struct ODE_system {
-  ODE_system(Cell* cell, real_t a, real_t b, real_t c, const std_map_bdm_dg_t& dg_map)
-  : c_(cell), alpha_(a), beta_(b), gamma_(c), dg_(dg_map) {}
+class ODE_parameters {
+ public:
+  ODE_parameters(real_t a, real_t b, real_t c)
+  : alpha_(a), beta_(b), gamma_(c) {}
+  ODE_parameters(const ODE_parameters& p) {
+    alpha_ = p.alpha(); beta_ = p.beta(); gamma_ = p.gamma();
+  }
+
+  inline real_t alpha() const { return alpha_; }
+  inline real_t beta()  const { return beta_; }
+  inline real_t gamma() const { return gamma_; }
+
+ private:
+  real_t alpha_, beta_, gamma_;
+};
+
+struct ODE_system : public ODE_parameters {
+  ODE_system(Cell* cell, const ODE_parameters& p, const std_map_bdm_dg_t& dg_map)
+  : ODE_parameters(p), c_(cell), dg_(dg_map) {}
 
   void operator()(const boost_vector_t& x, boost_vector_t& dxdt, real_t t) const {
     auto& crd = c_->GetPosition();
     auto& dg = dg_.find("cytokine")->second;
     const real_t ecm_c = dg->GetValue(crd);
 
-    dxdt[0] = alpha_ * x[1];
-    dxdt[1] = beta_ * x[1] - beta_ * x[1] * x[0] * x[0] - gamma_ * x[0];
+    dxdt[0] = alpha() * x[1];
+    dxdt[1] = beta() * x[1] - beta() * x[1] * x[0] * x[0] - gamma() * x[0];
   }
 
  private:
   Cell* c_;
-  real_t alpha_, beta_, gamma_;
   std_map_bdm_dg_t dg_;
 };
 
-struct ODE_jacobian {
-  ODE_jacobian(Cell* cell, real_t a, real_t b, real_t c, const std_map_bdm_dg_t& dg_map)
-  : c_(cell), alpha_(a), beta_(b), gamma_(c), dg_(dg_map) {}
+struct ODE_jacobian : public ODE_parameters {
+  ODE_jacobian(Cell* cell, const ODE_parameters& p, const std_map_bdm_dg_t& dg_map)
+  : ODE_parameters(p), c_(cell), dg_(dg_map) {}
 
   void operator()(const boost_vector_t& x, boost_matrix_t& jac, real_t t, boost_vector_t& dfdt) const {
     jac(0, 0) = 0.0;
-    jac(0, 1) = alpha_;
-    jac(1, 0) = -2.0 * beta_ * x[0] * x[1] - gamma_;
-    jac(1, 1) = beta_ - beta_ * x[0] * x[0];
+    jac(0, 1) = alpha();
+    jac(1, 0) = -2.0 * beta() * x[0] * x[1] - gamma();
+    jac(1, 1) = beta() - beta() * x[0] * x[0];
     //
     dfdt[0] = 0.0;
     dfdt[1] = 0.0;
@@ -57,7 +72,6 @@ struct ODE_jacobian {
 
  private:
   Cell* c_;
-  real_t alpha_, beta_, gamma_;
   std_map_bdm_dg_t dg_;
 };
 
@@ -100,10 +114,12 @@ inline int Simulate(int argc, const char** argv) {
 
   auto* rm = sim.GetResourceManager();
 
-  // agent-based model simulation time-step
+  // time-step of the BioDynaMo simulator
   const real_t dt_BDM = sim.GetParam()->simulation_time_step;
-  // regulatory network simulation time-step
+  // time-step of the regulatory network solver
   const real_t dt_RN = 1000.0;
+  // parameters of the regulatory network
+  const ODE_parameters rn_p(1.e+0, 1.e+3, 1.e+0);
   // BioDynaMo's diffusion grid sample points in each dimension
   int n_DG = 51;
 
@@ -119,8 +135,8 @@ inline int Simulate(int argc, const char** argv) {
   auto* cell = new Cell({0.01, 0.02, 0.03});
   cell->SetDiameter(1.0);
   cell->AddBehavior(new RegulatoryNetwork(dt_RN, {1., 1.},
-                                          ODE_system(cell,1,1000,1,dg_m),
-                                          ODE_jacobian(cell,1,1000,1,dg_m),
+                                          ODE_system(cell,rn_p,dg_m),
+                                          ODE_jacobian(cell,rn_p,dg_m),
                                           ODE_output(cell,dg_m)));
 
   rm->AddAgent(cell);
