b3bf419743e495c7ebcd11dce28f2d5ab5c78ebf
Vasileios Vavourakis <vasvav@gmail.com>
2026-04-16 21:24:47 +0300

    added an interesting MATLAB script

---
+++ process_data.m
@@ -0,0 +1,22 @@
+
+clear, clc, close all
+
+if false
+    data = readmatrix('sine.csv');
+
+    figure, hold on
+    plot(data(:,1),data(:,2),'r-')
+    plot(data(1,1),data(1,2),'r*')
+    axis equal
+    hold off
+end
+
+if true
+    data = readmatrix('lorenz.csv');
+    
+    figure, hold on
+    plot3(data(:,2),data(:,3),data(:,4),'r-')
+    plot3(data(1,2),data(1,3),data(1,4),'r*')
+    axis equal
+    hold off
+end
